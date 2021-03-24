56520611ec9433b1a297791f5e5fa446431752aa
KlayGomez <80277066+KlayGomez@users.noreply.github.com>
2021-03-24 10:45:38 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@
 var btn=document.getElementById("sensor");
 
 function LED1_On() {
-	alert(bnt);
+	alert(btn);
 	console.log("led on");
 	document.getElementById("sensor").innerHTML="led on";
 	client.subscribe("klay-2000@outlook.com/tema1");
