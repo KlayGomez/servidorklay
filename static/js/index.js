59c95a5365b29d2eae4831d3b714257d3616dbe4
KlayGomez <80277066+KlayGomez@users.noreply.github.com>
2021-03-24 10:21:30 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
 //https://www.eclipse.org/paho/clients/js/
 
 function LED1_On(mensaje) {
-	alert(mensaje..payloadString);
+	alert(mensaje.payloadString);
 	console.log("led on");
 	document.getElementById("sensor").innerHTML="led on";
 	client.subscribe("klay-2000@outlook.com/tema1");
